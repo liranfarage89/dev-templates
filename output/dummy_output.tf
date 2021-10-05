b9d1529cd03fdded5fa0845bda0acc437a18fdbe
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2021-10-05 12:55:59 +0300

    Update dummy_output.tf

--- output/dummy_output.tf
+++ output/dummy_output.tf
@@ -2,10 +2,3 @@ output "var_output" {
   value = "something_new"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "migration-remote-backend"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-  }
-}
