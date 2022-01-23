232d51f9b9747cdc6bc7ff0f5b1ae3194f42b808
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-01-23 17:28:04 +0200

    Create variables.tf

---
+++ s3-bucket/variables.tf
@@ -0,0 +1,31 @@
+variable "access_key" {
+     description = "Access key to AWS console"
+}
+variable "secret_key" {
+     description = "Secret key to AWS console"
+}
+variable "region" {
+     description = "Region of AWS VPC"
+}
+
+variable "aws_session_token" {
+  description = "Temporary session token used to create instances"
+}
+
+variable "bucket_name" {
+  description = "(Required) Creates a unique bucket name"
+  type        = string
+  default     = "test-bucket-liran-delete"
+}
+
+variable "force_destroy" {
+  description = "(Optional) A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
+  type        = string
+  default     = true
+}
+
+variable "tags" {
+  description = "(Optional) A mapping of tags to assign to the bucket"
+  type        = map
+  default     = {"env": "test"}
+}
