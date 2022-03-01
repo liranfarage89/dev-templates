e1b1cd3592db27c31d173b6cbe246c6be7372d15
Liran Farage <83922349+liranfarage89@users.noreply.github.com>
2022-03-01 10:39:45 +0200

    Update main.tf

--- variables/main.tf
+++ variables/main.tf
@@ -9,6 +9,16 @@ variable "test_sensitive_variable" {
   sensitive = true
 }
 
+variable "ebs_size" {
+  type    = number
+  default = 1
+}
+
+variable "is_ready" {
+  type    = boolean
+  default = true
+}
+
 output default_test_var {
   value = var.test_variable
   
